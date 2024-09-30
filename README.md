55cd9c78c69838ca18111e7b7657f698d99a5ee2
joypauls <joypaulsen3@gmail.com>
2024-09-29 21:26:24 -0500

    playing around with ui

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 # Wavelet Kit
 
-A tool for image deconvolution focused on planetary imaging
+A tool for wavelet-based image deconvolution focused on the planetary imaging use case.
 
 <div align="center">
     <img width="60%" src="./morlet_wavelet_visual.svg">
 </div>
+
+The [lucky imaging](https://en.wikipedia.org/wiki/Lucky_imaging) method is used by both amateur and professional astronomers to compensate for the effects of atmospheric seeing when imaging objects from ground-based telescopes. This method involves taking many short-exposure images, stacking the best ones to improve the signal-to-noise ratio, and applying deconvolution to sharpen the stacked image. Wavelet Kit offers tools meant to enhance the experience of the deconvolution process.
 
 ## Experimental Results
 
--- src/app.py
+++ src/app.py
@@ -1,24 +1,37 @@
 import streamlit as st
 import cv2
+
 # import numpy as np
 # from PIL import Image
 
+DEFAULT_LAYERS = 6
+MIN_LAYERS = 1
+MAX_LAYERS = 8
+MIN_LAYER_GAIN = 0.0
+MAX_LAYER_GAIN = 10.0
+
+
 def build_sidebar():
-    st.sidebar.title("Image Color Space Viewer")
-    # st.sidebar.markdown(
-    #     """
-    #     This app allows you to view an image in different color spaces. 
-    #     You can select the color space from the dropdown menu.
-    #     """
-    # )
-    color_space = st.sidebar.selectbox(
-        "Select algorithm:", ["Wavelet 1", "Wavelet 2"]
+    hide_elements = """
+        <style>
+            div[data-testid="stSliderTickBarMin"],
+            div[data-testid="stSliderTickBarMax"] {
+                display: none;
+            }
+        </style>
+    """
+    st.markdown(hide_elements, unsafe_allow_html=True)
+
+    wavelet = st.sidebar.selectbox("Select Wavelet:", ["B-Spline"])
+    layers = st.sidebar.number_input(
+        "Layers", MIN_LAYERS, MAX_LAYERS, DEFAULT_LAYERS, 1
     )
-    st.sidebar.markdown("Layers")
-    st.sidebar.slider("Layer 1", 0.0, 1.0, 0.5)
-    st.sidebar.slider("Layer 2", 0.0, 1.0, 0.5)
-    st.sidebar.slider("Layer 3", 0.0, 1.0, 0.5)
-    return color_space
+    layer_gain_values = []
+    for i in range(layers):
+        layer_gain_values.append(
+            st.sidebar.slider(f"Layer {i+1}", MIN_LAYER_GAIN, MAX_LAYER_GAIN, 1.0, 0.1)
+        )
+    return wavelet, layer_gain_values
 
 
 # Streamlit app
@@ -29,13 +42,14 @@ def main():
     test_image_path = "./src/images/stacked_jupiter.tiff"
     image = cv2.imread(test_image_path)
 
-    build_sidebar()
+    wavelet, layer_gain_values = build_sidebar()
 
     if image is None:
         st.error("Failed to load the test image. Please check the path.")
         return
 
     if image is not None:
+        st.markdown(layer_gain_values)
         st.image(
             cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
             caption="Original Image",
